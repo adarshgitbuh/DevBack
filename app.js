834173b47de84889f9d8623368af7fcdb89b889f
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-06-08 14:42:49 +0530

    Refactor user route handlers for improved structure and logging

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userID/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "shetty", lastName: "adhu" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("thios is the route handler");
+    // //Route handler
+    //res.send("Route handler one!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This the 2nd route handler");
+    //res.send("Route handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This the 3rd route handler");
+    // res.send("Route handler 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This the 4th route handler");
+    //res.send("Route handler 4");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
