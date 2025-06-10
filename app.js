53160656d4c400e554ca3acd3be729cced6506a3
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-06-10 11:13:02 +0530

    improve error handling

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
 
-const { userAuth, adminAuth } = require("./midlewares/auth");
-
-// Handle the Auth middlewares for Get, Post ,,,, delete
-app.use("/admin", adminAuth, (req, res, next) => {
-  res.send("Admin route accessed");
+app.get("/getData", (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error in /getData:", error);
+  }
 });
 
-app.get("/user", userAuth, (req, res) => {
-  console.log("User data requested");
-  res.send("User data retrieved successfully");
-});
-
-app.get("/admin/getAll", (req, res) => {
-  res.send("All admin data retrieved successfully");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
 });
 
 app.listen(3000, () => {
