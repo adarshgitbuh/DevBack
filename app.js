cae1a638d450f79b78453c5becec38d4437f2868
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-06-12 08:24:18 +0530

    Add user model and database connection; implement signup route

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const connectToDatabase = require("./config/database");
 const app = express();
+const User = require("./models/users");
 
-app.get("/getData", (req, res) => {
+app.post("/signup", async (req, res) => {
+  //create the new user instance
+  const user = new User({
+    firstName: "virat",
+    lastName: "SHETTHY",
+    email: "adarsh@123",
+    password: "123456",
+  });
+
   try {
+    await user.save();
+    res.send("User created successfully");
   } catch (error) {
-    console.error("Error in /getData:", error);
+    res.status(400).send("error creating user: " + error.message);
   }
 });
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Internal Server Error: " + err.message);
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+connectToDatabase()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to Database...:", error);
+  });
---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  await mongoose.connect(
+    "mongodb+srv://adarshint:DwJvkUtT7aX77YtU@adnodejs.kn3nw.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectToDatabase;
