a6a57e6d761260c89893e66f8f3175e6357361ac
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-06-17 12:00:30 +0530

    user schema validation rules

--- models/users.js
+++ models/users.js
@@ -1,29 +1,50 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    maxlength: 18,
+    minlength: 4,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: Number,
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "other"].includes(value)) {
+        throw new Error("Gender data not valid");
+      }
+    },
+  },
+  about: {
+    type: String,
+    default: "Hey there! I am using DevTinder",
+  },
+  skills: {
+    type: [String]
+  },
+},
+{
+  timestamps: true,
+}
+);
 
-
-    firstName: {
-        type: String,
-    },
-    lastName: {
-        type: String,
-    },
-    email: {
-        type: String,
-    },
-    password: {
-        type: Number,
-        type: String, 
-
-    },
-    age: {
-        type: Number,
-    },
-    gender: {
-        type: String,
-    },
-});
-
-                           
 module.exports = mongoose.model("User", userSchema);
